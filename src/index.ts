1769572b4840198540cf727f2472a380b1ee3fd8
Mansur Rizoev <75481284+mansurRizoev@users.noreply.github.com>
2025-04-21 14:34:50 +0500

    sdse

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { ExamplePlugin } from './definitions';
 
-const Example = registerPlugin<ExamplePlugin>('Example', {
-  web: () => import('./web').then((m) => new m.ExampleWeb()),
-});
+const Example = registerPlugin<ExamplePlugin>('Example');
 
 export * from './definitions';
 export { Example };
